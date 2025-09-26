74b36bc51f95a2b8168b093a44291e0777d9ae35
AbdurRahman11072 <rjrahman014@gmail.com>
2025-09-27 04:10:17 +0600

    Added Responsive Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/NavigationMenu/topNavbar";
+import NavigationMenu from "@/components/NavigationMenu/mainNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable}  bg-backgroundBG conttainer`}
+        className={`${geistSans.variable} ${geistMono.variable}  bg-background`}
       >
-        {children}
+        <NavigationMenu />
+        <main className="">{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="flex  flex-col justify-center items-center p-24 bg-primaryBg/10 w-[80%]  ">
-      <h1 className="text-3xl font-bold text-textBg">Hello, Next.js 15</h1>
-      <button className="mt-4 px-4 py-2 bg-primaryBg/80 text-textBg rounded">
-        Click Me
-      </button>
-    </div>
-  );
+  return <div></div>;
 }
